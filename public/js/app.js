bb86a82f1f7549a9f46e3fa1b4abbf778f551c0c
djnorrisdev <42302814+djnorrisdev@users.noreply.github.com>
2018-09-23 09:57:05 -0400

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+let menuBtn = document.getElementsByClassName('menu-btn')
+let mobileMenu = document.getElementsByClassName('mobile-menu')
+let clickedBtn = function(){
+    mobileMenu[0].classList.toggle('active')
+}
+menuBtn[0].addEventListener('click', clickedBtn)
